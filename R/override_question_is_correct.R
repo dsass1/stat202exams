1ad9975806e8986ecd72422ce13ab8e36df3e9be
dsass1 <dsass1@comcast.net>
2022-09-08 10:44:52 -0500

    add imports

--- R/override_question_is_correct.R
+++ R/override_question_is_correct.R
@@ -1,5 +1,12 @@
 #override tutorial completion
-
+#' @importFrom learnr question_ui_initialize
+#' @importFrom learnr question_ui_completed
+#' @importFrom learnr question_ui_try_again
+#' @importFrom learnr question_is_valid
+#' @import learnr
+#' @import ISDStutorials
+#' 
+#' 
 # Override to mark every question as wrong
 #' @export
 question_is_correct.blank <- function(question, value, ...) {
