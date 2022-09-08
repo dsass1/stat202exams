8171354b6e2ce003cb537ccf971ee0035a3bdf3d
dsass1 <dsass1@comcast.net>
2022-09-08 15:57:46 -0500

    update

--- R/override_question_is_correct.R
+++ R/override_question_is_correct.R
@@ -9,40 +9,40 @@
 
 #' @export
 question_is_correct.default <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.blank <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.wordbank <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.multidrop <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.matching <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.learnr_checkbox <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.learnr_numeric <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
 
 #' @export
 question_is_correct.learnr_radio <- function(question, value, ...) {
-  learnr::mark_as(FALSE)
+  return(learnr::mark_as(FALSE))
 }
